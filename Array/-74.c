23c6e1a0688b9a265e91c101356af8e78b79d9ee
chen1008 <jack20051008@gmail.com>
2024-11-16 21:09:01 +0800

    修改-74.c

--- Array/-74.c
+++ Array/-74.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-    int n,m,arr[100000]={0},remender[100000]={0};
+    int n,m;
     scanf("%d",&n);
+    int *arr = (int *)malloc(n * sizeof(int));
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     scanf("%d",&m);
+    int *remender = (int *)malloc(m * sizeof(int));
     for(int i=0;i<n;i++){
         remender[arr[i]%m]++;
     }
     for(int i=0;i<m;i++){
         printf("%d\n",remender[i]);
     }
+    free(arr);
+    free(remender);
 }
