9515d3769bc95815c40257603fc170ea7b74e823
chen1008 <jack20051008@gmail.com>
2024-11-18 14:56:24 +0800

    update code

--- CH14 結構/getDecimalValue.c
+++ CH14 結構/getDecimalValue.c
@@ -1,10 +1,28 @@
 #include "getDecimalValue.h"
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 // val : 紀錄0或1
 // next : 記錄下一個結構
 int getDecimalValue(struct ListNode* head)
 {
-    // add your code
+    if(head==NULL){
+        return 0;
+    }
+    int sum=0,current_2exp=1;
+    struct ListNode* temp=head;
+    struct ListNode* ptr=head;
+    while (temp->next!=NULL){
+        temp=temp->next;
+        current_2exp*=2;
+    }
+    while (1){
+        if(ptr->val==1){
+            sum+=current_2exp;
+        }
+        current_2exp/=2;
+        ptr=ptr->next;
+        if(ptr==NULL)break;
+    }
+    return sum;
 }
--- CH14 結構/oddEvenList.c
+++ CH14 結構/oddEvenList.c
@@ -1,8 +1,44 @@
 
 #include "oddEvenList.h"
- 
 struct ListNode* oddEvenList(struct ListNode* head)
 {
+    if (head == NULL) {  // 如果鏈結串列為空，直接返回空
+        return NULL;
+    }
+    int count=0;
+    struct ListNode* odd_head=NULL;
+    struct ListNode* even_head=NULL;
+    struct ListNode* odd_tail=NULL;
+    struct ListNode* even_tail=NULL;
+    struct ListNode* ptr=head;
     
+    while (ptr != NULL) {
+        count++;
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = ptr->val;
+        newNode->next = NULL;
+        if (count % 2 == 1) {  // 奇數
+            if (odd_head == NULL) {  // 如果奇數串列是空的，初始化頭尾
+                odd_head = odd_tail = newNode;
+            } else {  // 非空串列，加入新節點到尾部
+                odd_tail->next = newNode;
+                odd_tail = newNode;
+            }
+        } else {  // 偶數
+            if (even_head == NULL) {  // 如果偶數串列是空的，初始化頭尾
+                even_head = even_tail = newNode;
+            } else {  // 非空串列，加入新節點到尾部
+                even_tail->next = newNode;
+                even_tail = newNode;
+            }
+        }
+
+        ptr = ptr->next;  // 移動到下一個節點
+    }
+
+    if (odd_tail != NULL) {  // 如果奇數串列不空，將尾部接到偶數串列
+        odd_tail->next = even_head;
+    }
+    return odd_head;
 }
 
