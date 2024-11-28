37c77e429f1d341f9b9eba5d917df64216e284ef
chen1008 <jack20051008@gmail.com>
2024-11-28 21:06:22 +0800

    update

--- The_C_Programming/CH14_structure/getDecimalValue.c
+++ The_C_Programming/CH14_structure/getDecimalValue.c
@@ -9,20 +9,11 @@ int getDecimalValue(struct ListNode* head)
     if(head==NULL){
         return 0;
     }
-    int sum=0,current_2exp=1;
-    struct ListNode* temp=head;
+    int sum=0;
     struct ListNode* ptr=head;
-    while (temp->next!=NULL){
-        temp=temp->next;
-        current_2exp*=2;
-    }
-    while (1){
-        if(ptr->val==1){
-            sum+=current_2exp;
-        }
-        current_2exp/=2;
+    while (ptr!=NULL){
+        sum=sum*2+ptr->val;
         ptr=ptr->next;
-        if(ptr==NULL)break;
     }
     return sum;
 }
